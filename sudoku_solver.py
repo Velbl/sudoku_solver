fa4089fb85353e4bd9d9123f0341606e6bde9743
Stefan Velbl <velblstefan@gmail.com>
2021-08-30 19:40:33 +0200

    find_zero function.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,20 +1,18 @@
-sudoku_board = [
-    ["1", "2", "3", "1", "2", "3", "1", "2", "3"],
-    ["4", "5", "6", "4", "5", "6", "4", "5", "6"],
-    ["7", "8", "9", "7", "8", "9", "7", "8", "9"],
-    ["1", "2", "3", "1", "2", "3", "1", "2", "3"],
-    ["4", "5", "6", "4", "5", "6", "4", "5", "6"],
-    ["7", "8", "9", "7", "8", "9", "7", "8", "9"],
-    ["1", "2", "3", "1", "2", "3", "1", "2", "3"],
-    ["4", "5", "6", "4", "5", "6", "4", "5", "6"],
-    ["7", "8", "9", "7", "8", "9", "7", "8", "9"],
-]
+sudoku_board = [[8, 1, 0, 0, 3, 0, 0, 2, 7],
+                [0, 6, 2, 0, 5, 0, 0, 9, 0],
+                [0, 7, 0, 0, 0, 0, 0, 0, 0],
+                [0, 9, 0, 6, 0, 0, 1, 0, 0],
+                [1, 0, 0, 0, 2, 0, 0, 0, 4],
+                [0, 0, 8, 0, 0, 5, 0, 7, 0],
+                [0, 0, 0, 0, 0, 0, 0, 8, 0],
+                [0, 2, 0, 0, 1, 0, 7, 5, 0],
+                [3, 8, 0, 0, 7, 0, 0, 4, 2]]
 
 
-def print_sudoku_board(sudoku_board):
+def print_board(board):
     # entire number of rows and columns
-    num_row = len(sudoku_board)
-    num_col = len(sudoku_board[0])
+    num_row = len(board)
+    num_col = len(board[0])
 
     # number of rows and columns inside one block
     num_row_block = num_row / 3
@@ -28,22 +26,36 @@ def print_sudoku_board(sudoku_board):
         for col in range(num_col):
             if col % num_col_block == 0:
                 print("║", end=" ")
-            print(sudoku_board[row][col], end=" ")
+            print(board[row][col], end=" ")
         print("║")
     print(" ══════   ══════  ══════ ")
 
 
-def solve_sudoku(sudoku_board):
-    solution = solve_sudoku_wrapper(sudoku_board)
+def solve_sudoku(board):
+    solution = solve_sudoku_wrapper(board)
     return solution
 
 
-def solve_sudoku_wrapper(sudoku_board):
+def solve_sudoku_wrapper(board):
     solution = "Solution is not implemented."
     return solution
 
 
-print_sudoku_board(sudoku_board)
+def find_zero(board):
+    zero_position = [None, None]
+    num_row = len(board)
+    num_col = len(board[0])
 
-solution = solve_sudoku(sudoku_board)
-print(solution)
+    for row in range(num_row):
+        for col in range(num_col):
+            if board[row][col] == 0:
+                zero_position[0] = row
+                zero_position[1] = col
+                return zero_position
+
+
+print(find_zero(sudoku_board))
+# print(zero_position)
+
+# solution = solve_sudoku(board)
+# print(solution)
